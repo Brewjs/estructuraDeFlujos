06eebc953474cc15dd2c0dd6ce660d452008258c
Nicolas Labbe <nicoo.labbe@gmail.com>
2022-02-19 11:16:00 -0300

    nuevo scrip ciclos

---
+++ assets/js/scriptCiclos.js
@@ -0,0 +1,18 @@
+//estructuras de control de flujo
+// if - switch - for - while - do while
+// simbolos de comparacion: > < >= <= == != === !==
+
+//for
+//Solicita por pantalla 10 nombres e imprimelos en consolda
+
+var hasta = prompt('¿Cuantos nombres necesitas?')
+for (/*Inicio*/var i = 0; /*hasta*/i < hasta; /*de cuanto en cuanto*/i = i + 1) {
+    var nombre = prompt('ingresa tu nombre');
+    console.log(nombre);
+}
+
+//Contadores y acumuladores
+//contador = contador + 1
+// acumulador = acumulador + dato
+
+//Ciclo While
